9935e1d4713984624e345044ec96d0b3172cb5de
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-01-04 21:18:26 +0530

    more initial setup

---
+++ FaultIdentifier/FaultIdentifier/FaultIdentifier/GlobalUsingStatements.cs
@@ -0,0 +1,15 @@
+﻿global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using Microsoft.UI.Xaml.Controls.Primitives;
+global using Microsoft.UI.Xaml.Data;
+global using Microsoft.UI.Xaml.Input;
+global using Microsoft.UI.Xaml.Media;
+global using Microsoft.UI.Xaml.Media.Animation;
+global using Microsoft.UI.Xaml.Navigation;
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Runtime.InteropServices.WindowsRuntime;
+global using Windows.Foundation;
+global using Windows.Foundation.Collections;
